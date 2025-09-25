1ff237aae3a49d03816c9f7f9964d5cc8877e70e
LUISA DA SILVEIRA SOARES <04543012003@senacrs.edu.br>
2025-09-25 12:30:55 -0300

    iniciando desenvolvimento home

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const userType = localStorage.getItem('userType');
-    const messageElement = document.getElementById('user-type-message');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const userType = localStorage.getItem('userType');
+//     const messageElement = document.getElementById('user-type-message');
 
-    if (userType) {
-        let displayMessage = '';
+//     if (userType) {
+//         let displayMessage = '';
 
-        // 2. Definir a mensagem com base no tipo de usuário
-        if (userType.toLowerCase() === 'estudante') {
-            displayMessage = `Você é um usuário do tipo: ESTUDANTE`;
-        } else if (userType.toLowerCase() === 'professor') {
-            displayMessage = `Você é um usuário do tipo: PROFESSOR`;
-        } else if (userType.toLowerCase() === 'admin') {
-            displayMessage = `Você é um usuário do tipo: ADMINISTRADOR`;
-        } else {
-            displayMessage = `Tipo de usuário não reconhecido: ${userType}`;
-        }
+//         // 2. Definir a mensagem com base no tipo de usuário
+//         if (userType.toLowerCase() === 'estudante') {
+//             displayMessage = `Você é um usuário do tipo: ESTUDANTE`;
+//         } else if (userType.toLowerCase() === 'professor') {
+//             displayMessage = `Você é um usuário do tipo: PROFESSOR`;
+//         } else if (userType.toLowerCase() === 'admin') {
+//             displayMessage = `Você é um usuário do tipo: ADMINISTRADOR`;
+//         } else {
+//             displayMessage = `Tipo de usuário não reconhecido: ${userType}`;
+//         }
         
-        // 3. Exibir a mensagem na tela
-        messageElement.textContent = displayMessage;
-    }
-});
+//         // 3. Exibir a mensagem na tela
+//         messageElement.textContent = displayMessage;
+//     }
+// });
